813e19904d04f818290a9ca17b27766fc64df49c
IsaMontoya17 <isabelamontoya2021@gmail.com>
2026-01-07 20:46:21 -0500

    feat(products): add gateway request validation in Product controller

--- products-service/src/main/java/com/digitalhouse/products_service/controller/ProductRestController.java
+++ products-service/src/main/java/com/digitalhouse/products_service/controller/ProductRestController.java
@@ -1,17 +1,27 @@
 package com.digitalhouse.products_service.controller;
 
 import com.digitalhouse.products_service.entity.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value="/products")
+@RequestMapping("/products")
 public class ProductRestController {
 
-    @GetMapping()
-    public Product getProduct(@RequestParam String id) {
-        return new Product(id, "Notebook", 2000.0);
+    @GetMapping
+    public ResponseEntity<?> getProduct(
+            @RequestParam String id,
+            @RequestHeader(value = "X-Request-from", required = false) String requestFrom
+    ) {
+
+        if (requestFrom == null || !requestFrom.equals("gateway")) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Access denied: only gateway is allowed");
+        }
+
+        Product product = new Product(id, "Notebook", 2000.0);
+        return ResponseEntity.ok(product);
     }
 }
